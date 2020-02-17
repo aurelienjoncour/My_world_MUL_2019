31a478346f430cfb12de8f05b6f21be7f0170496
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-02-17 11:19:05 +0100

    feat(projet_iso_point.c) adding function

---
+++ include/my_world.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_world_2019
+** File description:
+** my_world
+*/
+
+#ifndef MY_WORLD_H_
+#define MY_WORLD_H_
+
+
+#define M_PI    3.1415926535
+#define DEG_TO_RAD(x) ((x * M_PI) / 180)
+
+#include <SFML/Graphics.h>
+#include <SFML/Config.h>
+#include <math.h>
+
+sfVector2f project_iso_point(int x, int y, int z, sfVector2f translation);
+
+#endif /* !MY_WORLD_H_ */
---
+++ src/project_iso_point.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_world_2019
+** File description:
+** project_iso_point
+*/
+
+#include "my_world.h"
+
+sfVector2f project_iso_point(int x, int y, int z, sfVector2f translation)
+{
+    sfVector2f point_2d = {0};
+    int angle_x = 45;
+    int angle_y = 35;
+
+    point_2d.x = cos(DEG_TO_RAD(45)) * x - cos(DEG_TO_RAD(45)) * y
+                + translation.x;
+    point_2d.y = sin(DEG_TO_RAD(35)) * y + sin(DEG_TO_RAD(35)) * x - z
+                + translation.y;
+    return point_2d;
+}
