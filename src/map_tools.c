d56768426a393b41f9e4bed7ff269db36d19025c
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-02-17 17:53:24 +0100

    feat(map_tools) changing value of map->modified

--- src/map_tools.c
+++ src/map_tools.c
@@ -10,6 +10,7 @@
 int map_rotate(map_t *map, sfVector2f *angle)
 {
     map->angle = *angle;
+    map->modified = sfTrue;
     return EXIT_SUCCESS;
 }
 
@@ -19,11 +20,13 @@ int map_scale(map_t *map, float scale)
     map->sampling.y += scale;
     if (map->sampling.y >= 0 && scale > 0)
         map->sampling.z += scale;
+    map->modified = sfTrue;
     return EXIT_SUCCESS;
 }
 
 int map_translate(map_t *map, sfVector2f *new_origin)
 {
     map->origin = *new_origin;
+    map->modified = sfTrue;
     return EXIT_SUCCESS;
 }
