46e9b53d5e60294ca268d0bc2f7da92945c23968
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-02-17 15:09:24 +0100

    fix(map_vertex_create_destroy) adding missing bracket

--- src/map/map_vertex_create_destroy.c
+++ src/map/map_vertex_create_destroy.c
@@ -31,8 +31,8 @@ static sfVertexArray *create_line(sfVector2f *point1, sfVector2f *point2)
 
 static int map_vertex_malloc(map_t *map)
 {
-    map->vrtx_x = malloc(sizeof(sfVertexArray *) * (map->width * map->height);
-    map->vrtx_y = malloc(sizeof(sfVertexArray *) * (map->width * map->height);
+    map->vrtx_x = malloc(sizeof(sfVertexArray *) * (map->width * map->height));
+    map->vrtx_y = malloc(sizeof(sfVertexArray *) * (map->width * map->height));
     if (!map->vrtx_x || !map->vrtx_y) {
         my_putstr_error("map_vertex_create: malloc error\n");
         return EXIT_FAILURE;
