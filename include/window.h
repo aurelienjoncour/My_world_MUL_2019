6c43c1396952e2d3a4c07a7d2c2ebe05fa4acfd0
RACAUD Simon <simonracaud@gmail.com>
2020-02-17 16:49:28 +0100

    FEAT(window.h) : create window struct

---
+++ include/window.h
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_world_2019
+** File description:
+** map_t
+*/
+
+#ifndef WINDOW_H_
+#define WINDOW_H_
+
+#include "my_world.h"
+
+enum projection {
+    ISOMETRIC,
+    PARALLEL
+};
+
+typedef struct window {
+    map_t map;
+    sfRenderWindow *window;
+    enum projection mode;
+    sfBool is_fullscreen;
+    sfBool reload_window;
+    int exit_status;
+    int width;
+    int height;
+} window_t;
+
+#endif
