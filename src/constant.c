1c0875f7049b5f3501ab11aa11220df9589fad47
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-02-17 17:08:09 +0100

    fix(constant.c) rm useless extern

--- src/constant.c
+++ src/constant.c
@@ -10,7 +10,7 @@
 const int WIN_HEIGHT = 1080;
 const int WIN_WIDTH = 1920;
 const int WIN_BPP = 32;
-extern const int WIN_BIN_BPP;
+
 const sfVector3f MAP_INIT_SAMP = {64, 64, 10};
 const sfVector2f MAP_INIT_ANGL = {45, 35};
 const sfVector2f MAP_INIT_ORIG = {WIN_WIDTH / 2, WIN_HEIGHT / 2};
