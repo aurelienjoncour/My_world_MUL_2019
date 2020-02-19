872846ddf2ac741985357fb1db1f86e0b48ad329
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-02-19 12:09:20 +0100

    feat adding button set size function

--- src/input/button/button_set_size.c
+++ src/input/button/button_set_size.c
@@ -4,3 +4,14 @@
 ** File description:
 ** set state button
 */
+
+#include "my_world.h"
+#include "button.h"
+
+int button_set_size(button_t *button, sfVector2f *size)
+{
+    if (size->x <= 0 || size->y <= 0)
+        return EXIT_FAILURE;
+    button->size = *size;
+    return EXIT_SUCCESS;
+}
