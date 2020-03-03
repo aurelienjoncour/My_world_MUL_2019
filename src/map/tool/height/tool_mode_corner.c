4adbec860f6b07a9e9b49a6d98693b9d146f523b
RACAUD Simon <simonracaud@gmail.com>
2020-03-03 16:25:54 +0100

    Fix(Map tool edit corner mode) : focus point

--- src/map/tool/height/tool_mode_corner.c
+++ src/map/tool/height/tool_mode_corner.c
@@ -7,10 +7,10 @@
 
 #include "my_world.h"
 
-int corner_mode(map_t *map, sfVector2f *mouse, int coefficient)
+static sfVector2i search_point(map_t *map, sfVector2f *mouse)
 {
     float distance = point_length(*mouse, map->map_2d[0][0]);
-    sfVector2i save = {0};
+    sfVector2i save;
 
     for (int i = map->height - 1; i >= 0; i--) {
         for (int j = map->width - 1; j >= 0; j--) {
@@ -21,6 +21,22 @@ int corner_mode(map_t *map, sfVector2f *mouse, int coefficient)
             }
         }
     }
+    return save;
+}
+
+int corner_mode(map_t *map, sfVector2f *mouse, int coefficient)
+{
+    static sfVector2i save = {0};
+    static sfVector2f prev_mouse = {0};
+    float distance_last_click = point_length(*mouse, prev_mouse);
+
+    prev_mouse = *mouse;
+    if (distance_last_click < map->sampling.x) {
+        map->map_3d[save.y][save.x] += coefficient;
+        map->modified = true;
+        return EXIT_SUCCESS;
+    }
+    save = search_point(map, mouse);
     if (coefficient == 0)
         coefficient = get_dynamical_coefficient(map->map_3d[save.y][save.x]);
     map->map_3d[save.y][save.x] += coefficient;
